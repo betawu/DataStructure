3482ecb04b42c6c64e3f56907eb2a69ba1abcd8d
吴宇轩 <beta@wuyuxuandeMacBook-Pro.local>
2018-05-29 23:37:35 +0800

    二分搜索树

--- src/com/binarysearchtree/BST.java
+++ src/com/binarysearchtree/BST.java
@@ -50,15 +50,57 @@ public class BST<E extends Comparable<E>> {
 	//添加的递归算法
 	private Node recursionAdd(Node node , E e) {
 		if(node == null) {
+			size++;
 			return new Node(e);
 			//对等于情况不做处理默认所有数据都不相等
 		}else if(node.data.compareTo(e)==-1){
-			node.left = recursionAdd(root.left, e);
-			return node.left;
-		}else {
 			node.right = recursionAdd(node.right, e);
-			return node.right;
+		}else if(node.data.compareTo(e)==1){
+			node.left = recursionAdd(node.left, e);
+		}
+		return node;
+	}
+	
+	public boolean contains(E e) {
+		return contains(root,e);
+	}
+
+	private boolean contains(BST<E>.Node node, E e) {
+		if(node == null) {
+			return false;
+		}
+		
+		if(node.data.compareTo(e) == 0) {
+			return true;
+		}else if(node.data.compareTo(e) == -1) {
+			return contains(node.left,e);
+		}else {
+			return contains(node.right,e);
 		}
 	}
 	
+	//二叉树的前序遍历
+	public void preOrder() {
+		preOrder(root);
+	}
+
+	private void preOrder(BST<E>.Node node) {
+		if(node == null) {
+			return;
+		}
+		
+		System.out.println(node.data);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {5,3,6,8,4,2};
+		BST<Integer> bst = new BST<>();
+		for (int i : arr) {
+			bst.add(i);
+		}
+		
+		bst.preOrder();
+	}
 }
