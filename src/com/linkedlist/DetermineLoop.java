0803c013a19ccba0b837cc5c38d7410e500e1fe7
Beta <541737447@qq.com>
2019-02-14 13:31:40 +0800

    Create DetermineLoop.java
    判断链表是否有循环

---
+++ src/com/linkedlist/DetermineLoop.java
@@ -0,0 +1,28 @@
+package com.linkedlist;
+
+/**
+*使用快慢指针判断链表是否有循环
+*/
+public class DetermineLoop {
+  //算法不允许产生任务实例
+  private DetermineLoop(){}
+  
+  static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+  }
+  
+  private static boolean HasLoop(ListNode node) {
+        ListNode p = node;
+        ListNode q = node;
+        while (p != null && q != null && q.next != null) {
+            p = p.next;
+            q = q.next.next;
+            if (p == q) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
